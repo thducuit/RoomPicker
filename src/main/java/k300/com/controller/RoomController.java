eae741cab826475443afb320d5cb678bd27aa1d2
unknown <thducuit@gmail.com>
2019-05-11 07:55:07 +0700

    init project

---
+++ src/main/java/k300/com/controller/RoomController.java
@@ -0,0 +1,14 @@
+package k300.com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RoomController {
+	
+	
+	
+	public String index() {
+	    return "index";
+	}
+}
